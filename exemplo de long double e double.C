f1fa6a324cd425e059dc298c38b242dafecebde6
Rikelme <rikelmemini100@gmail.com>
2026-05-31 22:56:31 -0300

    Subindo nos projetos

---
+++ exemplo de long double e double.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+    double preciseNumber = 3.141592653589793;
+    long double veryPreciseNumber = 3.14159265358979323846;
+ 
+    printf("Número preciso (double): %.15f\n", preciseNumber);
+    printf("Número muito preciso (long double): %.21Lf\n", veryPreciseNumber);
+ 
+    return 0;
+}
